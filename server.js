6f440edb8406a946e2c1f016bfaa3858a3a2d9d2
Omar S <omarspikes6@gmail.com>
2019-08-08 21:46:18 -0400

    server commit

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+let tablesarr = [
+    {
+        table: 'table'
+    }
+];
+
+app.get("/api/tables", function(req, res) {
+    res.json(tablesarr);
+})
+
+app.listen(PORT, function() {
+    console.log("server listen on port "+ PORT);
+})
+
